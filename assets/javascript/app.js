9e438bd09ddd15c877dab219d00e297974fedc6f
Andreya <avalabek@yahoo.com>
2017-12-05 10:28:39 -0700

    gifs remain still

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,36 +3,41 @@ $(document).ready(function() {
 
     // empty array of animals to be added to
     var animals = [];
+    
+
+
+        
 
     // Event listener for all button elements
     $("button").on("click", function() {
       // In this case, the "this" keyword refers to the button that was clicked
-      
+      //wrap the what happens on click in a function to call for new buttons
+      // function displayAnimalGiphy() {
       var animal = $(this).attr("data-animal");
 
-      // Constructing a URL to search Giphy for the name of the animal who said the quote
+      // Constructing a URL to search Giphy for the name of the animal 
       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         animal + "&api_key=dc6zaTOxFJmzC&limit=10";
-        
+        //TO DO make new buttons work with giphys--find out why they don't
         // TO DO animate and still giphys make variables to hold url the below items mess up page
-
-        // var imageUrl = response.data.image_original_url;
+          // var imageUrl = response.data.image_original_url;
          // var stillUrl = imageURL.replace(/.gif/i, '_s.gif');
 
-      // Performing our AJAX GET request
+      // AJAX GET request
       $.ajax({
           url: queryURL,
           method: "GET"
         })
-        // After the data comes back from the API
+        // After the data comes back from the giphy API
         .done(function(response) {
-          // Storing an array of results in the results variable
+          // put the response in an array call it the results variable
           var results = response.data;
+          console.log(results);
 
           // Looping over every result item
           for (var i = 0; i < results.length; i++) {
 
-            // Only taking action if the photo has an appropriate rating
+            // Preventing anything not pg or g from showing up
             if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
               // Creating a div with the class "item"
               var gifDiv = $("<div class='item'>");
@@ -40,15 +45,16 @@ $(document).ready(function() {
               // Storing the result item's rating
               var rating = results[i].rating;
 
-              // Creating a paragraph tag with the result item's rating
+              // Creating a paragraph tag with the result item's rating to show rating
               var p = $("<p>").text("Rating: " + rating);
 
-              // Creating an image tag
+              // Creating an image tag for giphy
               var animalImage = $("<img>");
 
-              // Giving the image tag an src attribute of a proprty pulled off the
-              // result item
-              animalImage.attr("src", results[i].images.fixed_height.url);
+              // Giving the image tag an src attribute of a property pulled off the
+              // result item found in console to make it still at first 
+// Question to animate it must splice out the _still but where in code to do this
+              animalImage.attr("src", results[i].images.fixed_height_still.url);
 
               // Appending the paragraph and animalImage we created to the "gifDiv" div we created
               gifDiv.append(p);
@@ -60,6 +66,7 @@ $(document).ready(function() {
           }
         });
     });
+  // }) 
     //function to make buttons from form input
     function renderButtons() {
         // delete animals before adding new ones
@@ -68,8 +75,8 @@ $(document).ready(function() {
         for (var i = 0; i < animals.length; i++) {
             //generate buttons for each animal in array
             var a = $("<button>");
-            // add class of animal to button QUESTION do I need this
-            // a.addClass("animal");
+            // add class of animal to button 
+            a.addClass("animal");
             // add data attribute of data-animal to button
             a.attr("data-animal", animals[i]);
             //button text
@@ -88,15 +95,18 @@ $(document).ready(function() {
         renderButtons();
 
     });
+    // // Adding a click event listener to all elements with a class of "animal"
+       // $(document).on("click", ".animal", displayAnimalGiphy());
 
     // call render buttons to display intitial buttons
     renderButtons();
 
+
     // when .gif is clicked, then do this: incomplete 
-    $(".gif").click(function() {
+    $(document).click(".gif", function() {
         //variable to store image data-state in
         var state = $(this).attr("data-state");
-        //the below works
+        
         console.log(state);
 
         //check if the variable state is equal to still
@@ -112,7 +122,7 @@ $(document).ready(function() {
             // by removing.gif and replacing it with_s.gif
             var updateURL = $(this).attr("data-still");
             $(this).attr("src", 'src.replace(/\.gif/i, "_s.gif"');
-            // $(this).attr("src", updateURL);
+            $(this).attr("src", updateURL);
             $(this).attr("data-state", "still");
 
         }
